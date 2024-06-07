0aa0f08799c6a9f5be8f0bea2a6e4e5973ee0843
Mohd Saqlain <mohdsaqlain6017@gmail.com>
2024-06-07 10:41:41 +0530

    done

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const app = express();
+const cors = require('cors');
 const connectToMongo = require("./db/connection");
 const port = 5000;
 
 connectToMongo();
 app.use(express.json());
 app.use(require('./routes/product'))
+app.use(cors());
 
 app.get('/', async (req,res)=> {
     res.send("Hello World");
